7908cd155484def942473d70dccfda577d1995c1
3626zoe <168171585+3626zoe@users.noreply.github.com>
2025-03-27 22:23:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwincss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,10 +8,3 @@ export default defineConfig({
     tailwincss(),
   ],
 })
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> d51e5b035ccdc795ff48b934dd2ea216a19ee72e
