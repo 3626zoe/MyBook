bcd0be8dda127705f16fb21706a3f6dab0a6cfeb
3626zoe <168171585+3626zoe@users.noreply.github.com>
2025-03-27 22:16:43 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router"
 
 function Header({ title, slogan }) {
@@ -19,28 +18,4 @@ function Header({ title, slogan }) {
       </header>
    );
 }
-
-=======
-import { Link } from "react-router"
-
-function Header({ title, slogan }) {
-   return (
-      <header className="text-center flex flex-col items-center header">
-         <Link to="/">
-         <h2 className="text-white pt-12 pb-2 text-3xl font-bold">
-            {title}
-         </h2>
-         </Link>
-         
-         <p className="text-gray-400 text-opacity-80 text-base leading-relaxed xl:w-1/2 lg:w-3/4 mx-auto">
-            {slogan}
-         </p>
-         <div className="flex mt-6 justify-center">
-            <hr className="my-[25px] mx-auto w-[100px] border-0 border-t-[6px] border-[rgb(161,218,229)] opacity-100 rounded" />
-         </div>
-      </header>
-   );
-}
-
->>>>>>> d51e5b035ccdc795ff48b934dd2ea216a19ee72e
 export default Header;
