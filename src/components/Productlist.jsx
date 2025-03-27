986148a51083321b2d37a89f0e6da5f3fa691466
3626zoe <zoeychang29@gmail.com>
2025-03-27 10:26:27 +0800

    Initial commit

---
+++ src/components/Productlist.jsx
@@ -0,0 +1,14 @@
+import ProductItem from './ProductItem';
+import products from '../json/books.json'
+
+function ProductList() {
+   return (
+       <div className="p-6 md:p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+           {products.map((product) => (
+               <ProductItem key={product.ID} product={product} />
+           ))}
+       </div>
+   );
+}
+
+export default ProductList;
