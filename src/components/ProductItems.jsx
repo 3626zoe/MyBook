df39e5c95992df21890f6bb36360bc252f605c63
3626zoe <168171585+3626zoe@users.noreply.github.com>
2025-03-27 22:18:33 +0800

    Update ProductItems.jsx

--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router";
 
 function truncateText(text, length) {
@@ -35,42 +34,4 @@ function Bookitem({ book }) {
         </Link>
     </section>
   );
-=======
-import { Link } from "react-router";
-
-function truncateText(text, length) {
-  return text.length > length ? text.substring(0, length) + "..." : text;
-}
-
-function Bookitem({ book }) {
-  return (
-    <section>
-        <Link to={`/Books/${book.ID}`}>
-            <div className="card bg-gradient-to-br from-blue-50 to-white w-96 shadow-lg">
-                <figure className="px-10 pt-10 ">
-                <img
-                    src={book.cover}
-                    alt={book.title}
-                    className="rounded-xl h-60 w-40 "
-                />
-                </figure>
-                <div className="card-body">
-                    <h3 className="card-title self-center text-2xl">
-                        {book.title}
-                    </h3>
-                    <h4 className="text-md self-center">
-                        {book.author}
-                    </h4>
-                    <p className="text-sm p-4">
-                        {truncateText(book.summary, 70)}
-                    </p>
-                    <h3 className="text-sm self-center">
-                        ${book.price}
-                    </h3>
-                </div>
-            </div>
-        </Link>
-    </section>
-  );
->>>>>>> d51e5b035ccdc795ff48b934dd2ea216a19ee72e
 }export default Bookitem;
