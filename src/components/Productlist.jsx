9c677a94c2c6e3c0c38e8ad14e211e699341b263
3626zoe <168171585+3626zoe@users.noreply.github.com>
2025-03-27 22:18:58 +0800

    Update Productlist.jsx

--- src/components/Productlist.jsx
+++ src/components/Productlist.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ProductItem from './ProductItems';
 import products from '../json/books.json'
 
@@ -11,20 +10,4 @@ function ProductList() {
        </div>
    );
 }
-
-=======
-import ProductItem from './ProductItem';
-import products from '../json/books.json'
-
-function ProductList() {
-   return (
-       <div className="p-6 md:p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-           {products.map((product) => (
-               <ProductItem key={product.ID} product={product} />
-           ))}
-       </div>
-   );
-}
-
->>>>>>> d51e5b035ccdc795ff48b934dd2ea216a19ee72e
 export default ProductList;
